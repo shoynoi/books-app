0117e7b6ec5140b62f1dab6fcec6668848e97116
吉野輝 <shoynoi.jp@gmail.com>
2019-05-26 11:15:20 +0900

    add author to books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -53,6 +53,6 @@ class BooksController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def book_params
-      params.require(:book).permit(:title, :memo)
+      params.require(:book).permit(:title, :memo, :author)
     end
 end
