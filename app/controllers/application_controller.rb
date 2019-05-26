7848ce8a39417c82641954263187a40d6ca57dc4
吉野輝 <shoynoi.jp@gmail.com>
2019-05-26 15:47:30 +0900

    i18nの設定を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
+  private
+
+  def set_locale
+    I18n.locale = params[:locale]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :books
+  scope ":locale", locale: /#{I18n.available_locales.join("|")}/ do
+    resources :books
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
