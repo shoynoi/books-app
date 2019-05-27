98e95f24dded56746b1f2dc7149cdfb2799ffc04
吉野輝 <shoynoi.jp@gmail.com>
2019-05-27 10:01:17 +0900

    1ページ毎の表示件数を10件に制限

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,5 @@ class Book < ApplicationRecord
   mount_uploader :picture, PictureUploader
   validates :title, presence: true, length: { maximum: 30 }
   validates :memo, presence: true
+  paginates_per 10
 end
