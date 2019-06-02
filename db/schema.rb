db7755453e92cb5d9ce206b4fc2e23049114dfde
吉野輝 <shoynoi.jp@gmail.com>
2019-06-02 14:24:52 +0900

    Deviseを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_30_075805) do
+ActiveRecord::Schema.define(version: 2019_06_02_030329) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -36,6 +36,26 @@ ActiveRecord::Schema.define(version: 2019_05_30_075805) do
     t.date "reported_on"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.string "name", null: false
+    t.string "postcode", limit: 8
+    t.string "address"
+    t.text "biography"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
