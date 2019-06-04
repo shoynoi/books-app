0c09ae87b82bfba53d478afdbd0509f2962eeb99
吉野輝 <shoynoi.jp@gmail.com>
2019-06-04 17:07:41 +0900

    ユーザーの編集、登録ページを修正し、バリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,24 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+  attr_writer :postcode1, :postcode2
+  before_validation :set_postcode
+  validates :postcode, presence: true, format: { with: /\A\d{3}[-]\d{4}\z/, message: "の入力内容を確認してください" }
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :biography, presence: true
+
+  def postcode1
+    @postcode1 ||= postcode.present? ? postcode.split("-").first : nil
+  end
+
+  def postcode2
+    @postcode2 ||= postcode.present? ? postcode.split("-").last : nil
+  end
+
+  private
+
+  def set_postcode
+    self.postcode = postcode1.present? && postcode2.present? ? [postcode1, postcode2].join("-") : nil
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   scope ":locale", locale: /#{I18n.available_locales.join("|")}/ do
+    devise_for :users
     resources :books
     resources :reports
     resources :comments, except: [:index, :show]
