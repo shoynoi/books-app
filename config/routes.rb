370dae7246cade0db0ea1497519f5a16f9084308
吉野輝 <shoynoi.jp@gmail.com>
2019-06-08 11:55:40 +0900

    ユーザー登録後、ログイン画面にリダイレクトするように修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   scope ":locale", locale: /#{I18n.available_locales.join("|")}/ do
-    devise_for :users
+    devise_for :users, controllers: { registrations: "users/registrations" }
     resources :users, only: [:index, :show]
     resources :books
     resources :reports
