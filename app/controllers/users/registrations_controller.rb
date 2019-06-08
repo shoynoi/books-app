ea99d8f49d98b0a4f256df50164e899207057fb1
吉野輝 <shoynoi.jp@gmail.com>
2019-06-08 12:54:45 +0900

    ユーザー情報更新後、ユーザープロフィールページにリダイレクトするように修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -59,4 +59,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def after_inactive_sign_up_path_for(resource)
     new_user_session_path
   end
+
+  def after_update_path_for(resource)
+    user_path(resource)
+  end
 end
