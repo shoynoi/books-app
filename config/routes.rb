6c5568b7d73ec56c297f217d601a574ad865a5d9
吉野輝 <shoynoi.jp@gmail.com>
2019-06-15 15:34:34 +0900

    リダイレクト時にロケールが維持されるように修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, only: :omniauth_callbacks, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
   scope ":locale", locale: /#{I18n.available_locales.join("|")}/ do
+    get "omniauth/:provider" => "omniauth#localized", as: :localized_omniauth
     devise_for :users, skip: :omniauth_callbacks, controllers: {
       registrations: "users/registrations",
       confirmations: "users/confirmations",
