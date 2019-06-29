bb8d6083434ad314315a165537d01dee2cfe46b4
吉野輝 <shoynoi.jp@gmail.com>
2019-06-29 22:25:34 +0900

    日報のsystem testを追加

---
+++ test/system/reports_test.rb
@@ -0,0 +1,40 @@
+require "application_system_test_case"
+
+class ReportsTest < ApplicationSystemTestCase
+
+  def setup
+    user = users(:bob)
+    sign_in(user)
+    @report = reports(:report)
+  end
+
+  test "日報一覧が表示される" do
+    visit reports_path(locale: :ja)
+    assert_selector "h1", text: "日報一覧"
+  end
+
+  test "日報の作成ができる" do
+    visit new_report_path(locale: :ja)
+    fill_in "タイトル", with: "example title"
+    fill_in "作業日", with: I18n.l(Date.today)
+    fill_in "内容", with: "example description"
+    click_button "登録する"
+    assert_text "日報を作成しました"
+  end
+
+  test "日報の更新ができる" do
+    visit edit_report_path(@report, locale: :ja)
+    fill_in "タイトル", with: "update title"
+    fill_in "作業日", with: I18n.l(Date.today)
+    fill_in "内容", with: "update description"
+    click_button "更新する"
+    assert_text "日報を更新しました"
+  end
+
+  test "自身の登録した日報の削除ができる" do
+    visit reports_path(locale: :ja)
+    click_link "削除", href: report_path(@report, locale: :ja)
+    page.driver.browser.switch_to.alert.accept
+    assert_text "日報を削除しました"
+  end
+end
