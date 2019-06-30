59dc0d820e2cfff26eaedd6e30a86e6014193e65
吉野輝 <shoynoi.jp@gmail.com>
2019-06-30 15:21:36 +0900

    OmniAuthを用いたGitHub認証についてsystem testを作成

---
+++ test/system/omniauth_pages_test.rb
@@ -0,0 +1,27 @@
+require "application_system_test_case"
+
+class OmniauthPagesTest < ApplicationSystemTestCase
+
+  def setup
+    OmniAuth.config.mock_auth[:github] = nil
+    Rails.application.env_config["omniauth.auth"] = set_omniauth
+  end
+
+  test "GitHub連携でサインアップする" do
+    visit user_session_path(locale: :ja)
+    assert_difference "User.count", 1 do
+      click_link "GitHubでログイン"
+      assert_text "GitHub アカウントによる認証に成功しました。"
+    end
+  end
+
+  test "GitHub連携でサインインする" do
+    visit user_session_path(locale: :ja)
+    click_link "GitHubでログイン"
+    click_link "ログアウト"
+    assert_no_difference "User.count" do
+      click_link "GitHubでログイン"
+      assert_text "GitHub アカウントによる認証に成功しました。"
+    end
+  end
+end
